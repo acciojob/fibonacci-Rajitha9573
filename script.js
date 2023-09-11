a95d01d857fcd6bab69d20f405e6c9bc9469082c
Rajitha9573 <141991018+Rajitha9573@users.noreply.github.com>
2023-09-11 12:05:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function fibonacci(num) {
 // your code here
-	if(num==0){
+	if(num==1){
 		return 0;
 	}
-	else if(num==1){
+	else if(num==2){
 		return 1;
 	}
 	return fibonacci(n-1)+fibonacci(n-2);
