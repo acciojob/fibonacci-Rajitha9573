74e2368a19d164b6834470067c062024c915a3d0
Rajitha9573 <141991018+Rajitha9573@users.noreply.github.com>
2023-09-11 12:03:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+	if(num==0){
+		return 0;
+	}
+	else if(num==1){
+		return 1;
+	}
+	return fibonacci(n-1)+fibonacci(n-2);
 }
 
 module.exports = fibonacci;
